f500a07659eb040493d8682a9ad7d55e522fb31c
Alan Chuan <chuan2@wisc.edu>
2022-01-31 22:15:47 -0600

    added main pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import GlobalStyle from "./globalStyles"
+import { ThemeProvider } from "styled-components"
+import { LightTheme } from "./components/Themes"
+import { Route, Switch } from "react-router-dom"
+import { Main } from "./components/Main"
+import {AboutPage} from "./components/AboutPage"
+import {BlogPage} from "./components/BlogPage"
+import {SkillsPage} from "./components/SkillsPage"
+import {ProjectsPage} from "./components/ProjectsPage"
 function App() {
-  return <div>
-    App File
-    </div>
+  return <>
+  <GlobalStyle/>
+  <ThemeProvider theme = {LightTheme} >
+  <Switch>
+    <Route exact path = "/" component = {Main}/>
+    <Route exact path = "/about" component = {AboutPage}/>
+    <Route exact path = "/blog" component = {BlogPage}/>
+    <Route exact path = "/skills" component = {SkillsPage}/>
+    <Route exact path = "/projects" component = {ProjectsPage}/>
+
+  </Switch>
+
+  </ThemeProvider>
+  </>
+  
     
 }
 
